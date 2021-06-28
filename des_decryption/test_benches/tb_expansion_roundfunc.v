08d561406e0f97e044851791fac179bc98ee545d
GoRv PatidaR <81484884+gorv02@users.noreply.github.com>
2021-06-28 21:22:16 +0530

    Delete tb_expansion_roundfunc.v

--- des_decryption/test_benches/tb_expansion_roundfunc.v
+++
@@ -1,9 +0,0 @@
-
-module tb_expansion;
-reg [1:32] right_half;
-wire [1:48] expanded;
-			
-	Expansion_roundfunc exp (right_half,expanded);
-
-	assign right_half = 32'b10110001_00101001_01010010_00101010 ;
-endmodule
