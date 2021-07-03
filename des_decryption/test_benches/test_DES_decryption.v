64253dd537e12df6d591c55d281da367567991d0
Gourav Patidar <gourav.ranayara@gmail.com>
2021-07-03 12:24:40 +0530

    DES-Decryption -> DES (Encryption + Decryption)

--- des_decryption/test_benches/test_DES_decryption.v
+++
@@ -1,9 +0,0 @@
-module test_DESdecryption;
-reg [1:64] ciphertext, key;
-wire [1:64] plaintext;
-			
-	DESdecryption des (ciphertext, key, plaintext);
-
-	assign ciphertext = 64'b10000101_11101000_00010011_01010100_00001111_00001010_10110100_00000101 ;
-	assign key = 64'b00010011_00110100_01010111_01111001_10011011_10111100_11011111_11110001 ;
-endmodule
